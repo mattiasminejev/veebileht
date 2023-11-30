64b38198cf7c4006f23ab604585c9b85db640569
Kristo Krikmann <krikmann@ut.com>
2023-11-30 22:35:57 +0200

    scriptid yhte faili

--- nuppud.js
+++ nuppud.js
@@ -1,3 +1,4 @@
+//kaardi_nuppud
 var currentLocation = 0; 
         var locations = [
             "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2091.4744972843464!2d26.722176412337006!3d58.38539888610982!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46eb37b2e8a6ef3d%3A0xb1c21ea409a3845f!2sTartu%20%C3%9Clikooli%20Delta%20keskus!5e0!3m2!1set!2see!4v1699810918712!5m2!1set!2see",
@@ -11,3 +12,19 @@ var currentLocation = 0;
             mapIframe.src = locations[currentLocation];
         }
 //script by Chat GPT
+//vordlus_nuppud
+function hindl(newText) {
+    document.getElementById('hindl').innerText = newText;
+}
+
+function nimil(newText) {
+    document.getElementById('nimil').innerText = newText
+}
+
+function hindr(newText) {
+    document.getElementById('hindr').innerText = newText;
+}
+
+function nimir(newText) {
+    document.getElementById('nimir').innerText = newText
+}
