c26713e27e20c2c0807c8b3aee5a842388f2bc74
Kristo Krikmann <krikmann@ut.com>
2023-11-30 23:55:51 +0200

    javascript yhte faili

--- nuppud.js
+++ nuppud.js
@@ -17,3 +17,18 @@ var currentLocation = 0;
             mapIframe.src = locations[currentLocation];
         }
 //script by Chat GPT
+function hindl(newText) {
+    document.getElementById('hindl').innerText = newText;
+}
+
+function nimil(newText) {
+    document.getElementById('nimil').innerText = newText
+}
+
+function hindr(newText) {
+    document.getElementById('hindr').innerText = newText;
+}
+
+function nimir(newText) {
+    document.getElementById('nimir').innerText = newText
+}
